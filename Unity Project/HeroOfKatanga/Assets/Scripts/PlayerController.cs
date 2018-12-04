0326735084603db0c4b4f7b3d22e9d98dbcf14d1
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2018-12-04 14:30:54 +0100

    Player & PlayerController
    Added first scene with player and implemented a playerController

---
+++ Unity Project/HeroOfKatanga/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    float jumpHight = 2f;
+    bool grounded = true;
+
+    Rigidbody myRB;
+    Vector3 inputs = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+
+        if (inputs != Vector3.zero)
+            transform.forward = inputs;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            myRB.AddForce(Vector3.up * Mathf.Sqrt(jumpHight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        myRB.MovePosition(myRB.position + inputs * speed * Time.fixedDeltaTime);
+    }
+}
