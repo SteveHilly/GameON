4a1a0eb7d3e30243376d0dd700708dd70cf36355
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2018-12-05 13:13:12 +0100

    Connected Arrows with movement
    It is now possible to move the player with the UI arrows

--- Unity Project/HeroOfKatanga/Assets/Scripts/PlayerController.cs
+++ Unity Project/HeroOfKatanga/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     [SerializeField]
     float speed = 5f;
@@ -12,30 +13,61 @@ public class PlayerController : MonoBehaviour {
     float groundDistance = 0.2f;
     [SerializeField]
     bool grounded = false;
+    float moveDistance;
+
+    bool moveRight = false;
+    bool moveLeft = false;
+
 
     public LayerMask ground;
 
     Rigidbody myRB;
     Vector3 inputs = Vector3.zero;
     private Transform groundChecker;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         myRB = GetComponent<Rigidbody>();
         groundChecker = transform.GetChild(0);
-	}
+    }
 
     // Update is called once per frame
     private void Update()
     {
-        grounded = Physics.CheckSphere(groundChecker.position, groundDistance,ground,QueryTriggerInteraction.Ignore);
+        grounded = Physics.CheckSphere(groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
 
 
 
         inputs = Vector3.zero;
         inputs.x = Input.GetAxis("Horizontal");
-        Debug.Log(Input.GetAxis("Horizontal"));
+        //Debug.Log(Input.GetAxis("Horizontal"));
 
-        
+        if (moveRight)
+        {
+            if (moveDistance < 1)
+                moveDistance += 0.05f;
+        }
+        else
+        {
+            if (moveDistance > 0)
+                moveDistance -= 0.05f;
+        }
+
+        if (moveLeft)
+        {
+            if (moveDistance > -1)
+                moveDistance -= 0.05f;
+        }
+        else
+        {
+            if (moveDistance < 0)
+                moveDistance += 0.05f;
+        }
+
+        if (moveLeft & moveRight)
+            inputs = Vector3.zero;
+
+        inputs.x = moveDistance;
 
         if (inputs != Vector3.zero)
             transform.forward = inputs;
@@ -46,8 +78,29 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         myRB.MovePosition(myRB.position + inputs * speed * Time.fixedDeltaTime);
+        Debug.Log(moveDistance);
+    }
+
+    public void RightArrowButtonDown()
+    {
+        moveRight = true;
+    }
+
+    public void RightArrowButtonUp()
+    {
+        moveRight = false;
+    }
+
+    public void LeftArrowButtonDown()
+    {
+        moveLeft = true;
+    }
+
+    public void LeftArrowButtonUp()
+    {
+        moveLeft = false;
     }
 }
