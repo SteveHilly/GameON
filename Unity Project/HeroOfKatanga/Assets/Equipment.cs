5ea2bcb0b601cb03265df2a79d1351a6c665e599
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2018-12-06 17:07:26 +0100

    Fixed rock throw

--- Unity Project/HeroOfKatanga/Assets/Equipment.cs
+++ Unity Project/HeroOfKatanga/Assets/Equipment.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Equipment : MonoBehaviour {
+public class Equipment : MonoBehaviour
+{
 
     [SerializeField]
     //GameObject rock;
@@ -32,7 +33,11 @@ public class Equipment : MonoBehaviour {
 
         rock.transform.parent = null;
         rock.SendMessage("SetKinematic");
-        rock.GetComponent<Rigidbody>().AddForce(0, 0, 10f);
+        if (gameObject.transform.eulerAngles.y == 180 || gameObject.transform.eulerAngles.y == -180)
+            rock.GetComponent<Rigidbody>().AddForce(-500f, 0, 0);
+        //rock.GetComponent<Rigidbody>().AddRelativeForce(250f, 0, 0);
+        else   //rock.GetComponent<Rigidbody>().AddForce(-250f, 0, 0);
+            rock.GetComponent<Rigidbody>().AddForce(500f, 0, 0);
         RemoveItem(rock);
     }
 
