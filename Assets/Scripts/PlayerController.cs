48f4db0f1739702187ad25c5a1f411b43caab5d3
Hendrik <45393605+Grokblood@users.noreply.github.com>
2018-12-07 09:20:17 +0100

    Add files via upload
    This version has tu UI in it, hearts for health, round buttons with french text on them and two new scenes for start and game over screens.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    float jumpHight = 2f;
+    [SerializeField]
+    float groundDistance = 0.2f;
+    [SerializeField]
+    bool grounded = false;
+
+    public LayerMask ground;
+
+    Rigidbody myRB;
+    Vector3 inputs = Vector3.zero;
+    private Transform groundChecker;
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody>();
+        groundChecker = transform.GetChild(0);
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        grounded = Physics.CheckSphere(groundChecker.position, groundDistance,ground,QueryTriggerInteraction.Ignore);
+
+
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+
+        if (inputs != Vector3.zero)
+            transform.forward = inputs;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            myRB.AddForce(Vector3.up * Mathf.Sqrt(jumpHight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        myRB.MovePosition(myRB.position + inputs * speed * Time.fixedDeltaTime);
+    }
+}
