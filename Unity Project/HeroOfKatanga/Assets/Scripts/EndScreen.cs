5deae8013dc7badbbec36001ad0f92e3112a8ab2
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2018-12-11 11:15:10 +0100

    Added End & Start screen

---
+++ Unity Project/HeroOfKatanga/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour {
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Unity Project/HeroOfKatanga/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    float childCount = 0;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddChild()
+    {
+        childCount++;
+        if (childCount == 3)
+            GameEnd();
+    }
+
+    void GameEnd()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
