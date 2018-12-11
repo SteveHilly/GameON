57cd6e51eacb241c4cca53bb85b258a93f5f1654
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2018-12-11 11:52:13 +0100

    Added breakable wall

---
+++ Unity Project/HeroOfKatanga/Assets/BreakableWall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Rock")
+            Destroy(gameObject);
+    }
+}
