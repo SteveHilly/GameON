d0996a8aa1c7eefe3a1e034c23d1e7d0e335988e
Stevy <sp.hilberts@outlook.com>
2018-12-15 22:10:53 +0100

    added falling rock, spawner, health, memory cluttering prevention

---
+++ Unity Project/HeroOfKatanga/Assets/Scripts/FallRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallRock : MonoBehaviour {
+
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().health -= damage; // player loses health
+            Destroy(gameObject); // rock is removed from the game
+        }
+
+        if (other.CompareTag("Delete Rock"))
+        {
+            Destroy(gameObject); // rock is removed to prevent tons of objects cluttering memory
+        }
+    }
+}
