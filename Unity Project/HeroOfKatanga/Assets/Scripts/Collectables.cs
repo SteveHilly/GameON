adf9fc78b6959a9bc92c582f52387859ddaa26e0
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2018-12-18 13:43:00 +0100

    Added collectables and functions

---
+++ Unity Project/HeroOfKatanga/Assets/Scripts/Collectables.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CollectablesTrigger")
+        {
+            GameObject.FindGameObjectWithTag("GameController").SendMessage("AddScore", 1);
+            Destroy(gameObject);
+        }
+    }
+}
