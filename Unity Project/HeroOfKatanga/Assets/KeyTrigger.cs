bd7f5cf97f83d148660e52ebbbad5cafd7462194
Nils Grote <nils.jonas.grote@hs-furtwangen.de>
2019-01-20 15:11:09 +0100

    Added keys and function

---
+++ Unity Project/HeroOfKatanga/Assets/KeyTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTrigger : MonoBehaviour {
+
+    [SerializeField]
+    GameObject doorTarget;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            doorTarget.SetActive(false);
+            gameObject.SetActive(false);
+            Debug.Log("Key collectet");
+        }
+        else
+        {
+            Debug.Log(collision.gameObject.name);
+            Debug.Log("Not the Player");
+        }
+    }
+}
